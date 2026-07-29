eaeefdc0133eb83a63cf22ca7ccb7e84ff0dd2f6
Vũ Lê Bình <vubinh2843@gmail.com>
2026-07-29 13:52:33 +0700

    build: refresh Mission Control dashboard

---
+++ dashboard/dist/assets/swimlanesDiagram-G3AALYLV-DxPyTBfL.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-B9yST3Nx.js";import{a as o}from"./mermaid.core-C7xoauwo.js";import"./transform-C7htkucW.js";import"./chunk-5VM5RSS4-g7uvd8Zz.js";import"./chunk-XXDRQBXY-Dxpvy8f2.js";import"./chunk-VR4S4FIN-CrG3k6p3.js";import"./chunk-32BRIVSS-83NniLhR.js";import"./Architecture-BBQtAsXg.js";import"./index-C_SwfZ0E.js";import"./crosshair-C41GdiPW.js";import"./minimize-2-Dm_r5fie.js";import"./maximize-2-BARj9ttE.js";import"./square-terminal-B_1lj-Ay.js";import"./channel-w23Lj6CR.js";import"./step-CWvwoXpJ.js";import"./string-CY7Nc7fS.js";var m=o(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),a=m,$=r({defaultLayout:"swimlane",styles:a});export{$ as diagram};
